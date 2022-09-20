0d7253c51992ed5afe72c966eb45b486a52ad1ba
ernestizic <ieifeanyichukwu@gmail.com>
2022-09-20 13:26:49 +0100

    test: added some test cases

--- src/tests/ConverterForm.test.js
+++ src/tests/ConverterForm.test.js
@@ -2,7 +2,6 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ConverterForm from '../components/ConverterForm';
 
-
 describe('Conversion form component', () => {
 
     test('Submit button should be disabled on initial render', ()=> {
@@ -14,7 +13,7 @@ describe('Conversion form component', () => {
     test('convert-from field should be rendered', ()=> {
         render(<ConverterForm currencySymbols={[{code: 'USD'}, {code: 'AUD'}]} />)
 
-        const fromSelectField = screen.getByRole("button", {name: /Convert from AUD/i});
+        const fromSelectField = screen.getByRole("button", {name: /Convert from/i});
         expect(fromSelectField).toBeInTheDocument()
     })
 
@@ -41,8 +40,8 @@ describe('Conversion form component', () => {
         const convertFromSelectField = screen.getByRole("button", {name: /convert from/i});
         expect(convertFromSelectField.textContent).toBe("AUD")
     })
-
     
+    // Amount field should be empty on initial render
     test('amount field should be empty on initial render', ()=> {
         render(<ConverterForm currencySymbols={[{code: 'USD'}, {code: 'AUD'}]} />)
 
@@ -51,14 +50,54 @@ describe('Conversion form component', () => {
     })
 
 
+    // FORM USER EVENTS
+    test("CONVERT FROM select box should bring out options when clicked and select an option", async()=> {
+      const user = userEvent.setup()
+      render(<ConverterForm currencySymbols={[{code: 'USD'}, {code: 'AUD'}]} />)
 
-    test('result should be displayed', async()=> {
+      const selectBox = screen.getByLabelText(/convert from/i)
+      await user.click(selectBox)
+      expect(screen.getAllByRole("option")).toHaveLength(2)
+
+      const selectedOption = screen.getByRole("option", {name: 'USD'})
+      await user.click(selectedOption)
+      expect(selectedOption).toBeTruthy()
+    })
+
+    test("CONVERT TO select box should bring out options when clicked and select an option", async()=> {
+      const user = userEvent.setup()
+      render(<ConverterForm currencySymbols={[{code: 'USD'}, {code: 'AUD'}]} />)
+
+      const selectBox = screen.getByLabelText(/convert to/i)
+      await user.click(selectBox)
+      expect(screen.getAllByRole("option")).toHaveLength(2)
+
+      const selectedOption = screen.getByRole("option", {name: 'USD'})
+      await user.click(selectedOption)
+      expect(selectedOption).toBeTruthy()
+    })
+
+
+    test('Submit button should be enabled when the input fields are filled', async () => {
         const user = userEvent.setup()
-        render(<ConverterForm currencySymbols={[{code: 'USD'}, {code: 'AUD'}]} />)
+        render(<ConverterForm currencySymbols={[{code: 'USD'}, {code: 'AUD'}, {code: 'NGN'}]} />)
+
+        // convert from
+        await user.click(screen.getByLabelText(/convert from/i))
+        await user.click(screen.getByRole("option", {name: 'USD'}))
+
+        // Convert to
+        await user.click(screen.getByLabelText(/convert to/i))
+        await user.click(screen.getByRole("option", {name: 'NGN'}))
+
+        // amount
+        await user.type(screen.getByRole("textbox", {name: /amount/i}), "300")
+
+        // Submit button should become enabled
+        expect(await screen.findByRole("button", {name: /get exchange rate/i})).toBeEnabled()
 
         const submitButton = screen.getByRole("button", {name: /get exchange rate/i})
         await user.click(submitButton)
-
-        expect(screen.getByTestId("result")).toBeInTheDocument()
     })
+
 });
